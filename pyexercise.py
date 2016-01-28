744cc738a63895e495ff713bcdeca511dd19b4fb
Hing Ming Xuan <1205182@1utar.my>
2016-01-28 17:18:06 +0800

    Added pyexercise.py

--- pyexercise.py
+++ pyexercise.py
@@ -1,24 +1,24 @@
 import sympy as sy
 import numpy as np
 
-def fun_1( your_id ):
-    ans = hex(your_id)
+def fun_1( my_id ):
+    ans = hex(my_id)
     return ans
 
 def my_integral():
     x = sy.Symbol('x')
-    ans = sy.integrate( sy.exp(-x**2), (x,0, sy.oo))
+    ans = sy.integrate( (sy.exp(-x)*(sy.sin(x)/x)), (x,0, sy.oo) )
     return ans
 
 def my_solution():
-    A = np.array( [[3,1], [1,2]] )
-    b = np.array([9,8])
+    A = np.array( [[3,1,2,3,1,2,5,1,1,2],[3,2,2,1,4,1,1,1,2,1],[1,2,3,4,3,2,1,2,3,4],[2,2,3,1,4,5,3,1,1,2],[2,3,4,1,2,3,2,4,5,3],[1,4,3,2,3,1,2,1,3,2],[1,3,2,1,2,5,4,3,2,1],[3,1,2,4,1,5,3,2,2,2],[2,1,1,1,4,3,1,3,2,4],[1,1,1,4,2,2,3,4,1,3]] )
+    b = np.array([2,6,5,1,3,3,4,9,8,2])
     x = np.linalg.solve(A,b) # Solve Ax = b
     return x
 
 
 if __name__ == '__main__':
-    your_id = 12345678
-    print('Hexadecimal representation of %d is %s'%( your_id, fun_1( your_id) ))
+    my_id = 1205182
+    print('Hexadecimal representation of %d is %s'%( my_id, fun_1( my_id) ))
     print('Integral = ', my_integral())
     print('Solution = ', my_solution())
